7ff9878b63de59035fe8b2f0be1537332277a6e3
carlfre <carlfrederiknk@gmail.com>
2024-08-22 14:45:50 +0200

    problem 1.1

--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def mesh_function(f, t):
-    pass
+def mesh_function(f: np.ndarray, t: np.ndarray) -> np.ndarray:
+    return f(t)
 
-def func(t):
-    pass
+def func(t: float) -> float:
+    return np.where((0 <= t) & (t <= 3), np.exp(-t), np.exp(-3*t))
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
